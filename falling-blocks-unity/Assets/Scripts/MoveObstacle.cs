67a5cbe53f771919771afddf2b12a51f559ba5b8
Rachel Lee <engrrachel318@gmail.com>
2024-12-16 20:43:45 +0900

    feature: set obstacle min max speed

--- falling-blocks-unity/Assets/Scripts/MoveObstacle.cs
+++ falling-blocks-unity/Assets/Scripts/MoveObstacle.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MoveObstacle : MonoBehaviour
 {
     float offsetY;
 
+    public Vector2 speedMinMax;
+    float speed;
+
     // Start is called before the first frame update
     void Start()
     {
         offsetY = transform.localScale.y / 2;
+
+        speed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, Difficulty.GetDiffPct());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(7 * Time.deltaTime * Vector3.down);
+        transform.Translate(speed * Time.deltaTime * Vector3.down, Space.World);
         if (transform.position.y + offsetY < -Camera.main.orthographicSize)
             Destroy(gameObject);
     }
